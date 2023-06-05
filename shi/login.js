a3a9c19c9fc00ec79cdca50e361e18c5ae8ead83
SoyAkira <112052914+SoyAkira@users.noreply.github.com>
2023-06-05 00:58:24 -0700

    Add files via upload

---
+++ shi/login.js
@@ -0,0 +1,9 @@
+    const iconEye = document.querySelector(".input");
+
+    console.log(iconEye);
+
+    iconEye.addEventListener("click", function () {
+        if (this.nextElementSibling.type === "password") {
+            this.nextElementSibling.type = "text";
+        }
+    });
